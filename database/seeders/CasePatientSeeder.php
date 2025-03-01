8efd4e4799fcef97dc8355a9cfdfa2637eefec09
theLastAirBender <fmihaja041@gmail.com>
2025-03-01 17:39:39 +0100

    seed attach patient and case fait

--- database/seeders/CasePatientSeeder.php
+++ database/seeders/CasePatientSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 use App\Models\CasePatient;
+use App\Models\Patient;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,21 @@ class CasePatientSeeder extends Seeder
     public function run(): void
     {
         //
-        CasePatient::factory()->count(50)->create();
+        $patientCount = Patient::count();
+        
+        if ($patientCount === 0) {
+            // Si aucun patient n'existe, en créer quelques-uns
+            Patient::factory(20)->create();
+        }
+        
+        // Récupère tous les patients
+        $patients = Patient::all();
+        
+        // Crée 50 cas patients
+        CasePatient::factory()->count(50)->create()->each(function ($casePatient) use ($patients) {
+            // Attache 1 à 3 patients aléatoires à chaque cas
+            $randomPatients = $patients->random(rand(1, 3));
+            $casePatient->patients()->attach($randomPatients->pluck('id')->toArray());
+        });
     }
 }
