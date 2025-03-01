b274821e9bf9f6dfa2730af37f9327b826e1e68e
Por-Noob06 <andorakoto175@gmai.com>
2025-03-01 13:21:44 +0300

    second commit

---
+++ database/migrations/2025_03_01_095239_contact.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Contact', function (Blueprint $table) {
+            $table->id();
+            $table->string('contact_type')->nullable();
+            $table->date('contact_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('Contact');
+    }
+};
